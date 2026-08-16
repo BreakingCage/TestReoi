c57157b31834d93ab916bd64fbdd925cb15f02dc
0KOUKEN0 <87186479+0KOUKEN0@users.noreply.github.com>
2026-08-16 14:53:13 +0900

    test: add code search diagnostic marker

---
+++ SearchDiagnostic.cpp
@@ -0,0 +1,8 @@
+// GitHub Code Search diagnostic file
+// Unique token: BC_PRIVATE_PUBLIC_SEARCH_DIAGNOSTIC_20260816
+
+class FBCCodeSearchDiagnostic_20260816
+{
+public:
+    static constexpr const char* Marker = "BC_PRIVATE_PUBLIC_SEARCH_DIAGNOSTIC_20260816";
+};
